95952846ac4541f9d81e54326b160591ed555401
jgollhardt <jgollhardt@gmail.com>
2017-05-20 22:18:21 -0400

    Organize shift data, add a button to export it to Google Calendar

--- fetch_data.js
+++ fetch_data.js
@@ -11,12 +11,12 @@ s.onload = function() {
 var s2 = document.createElement('script');
 s2.src = chrome.runtime.getURL('gapi_setup.js');
 s2.onload = function() {
-  // get Google client id from config
+  // Get Google client id from config
   var configURL = chrome.runtime.getURL('config.json');
   fetch(configURL).then(function(response) {
     return response.json();
   }).then(function(config) {
-    // initialize the gapi client, passing along the client id
+    // Initialize the gapi client, passing along the client id
     var event = new CustomEvent("handleClientLoad", {detail: config.CLIENT_ID});
     window.dispatchEvent(event);
   });
@@ -29,31 +29,68 @@ s2.onload = function() {
 // Grab the table containing schedule data
 var rows = document.getElementsByClassName("request_table_bordered")[0].rows;
 
-// First row is the date information
+// array of dates, will match indices with the shift info row
+var dates = [];
+// Grab date information from the first row
 var days = rows[0].cells;
 for (var i = 1; i < days.length; i++) {
   var dateCell = days[i].innerText.trim();
   // Splits the text into ["day", "date"]
   var dateInfo = dateCell.split("\n");
-  var day = dateInfo[0].trim();
   var date = dateInfo[1].trim();
-  console.log("The day is: " + day);
-  console.log("The date is: " + date);
+  dates.push(date);
 }
 
-// Third row is the shift information
+// Grab shift information from the third row
+// If a shift exists, match it with the date collected aboves
+var shiftEvents = [];
 var hours = rows[2].cells;
 for (var i = 1; i < hours.length; i++) {
   var shiftCell = hours[i].innerText.trim();
-  // if there's nothing in the cell, ignore it
+  // If there's a shift
   if (shiftCell != "") {
     // Splits the text into ["department", "hours", "location"]
-    // location appears to be a store identifier
+    // location is a store identifier
     var shiftInfo = shiftCell.split("\n");
     var dept = shiftInfo[0].trim();
     var shiftHours = shiftInfo[1].trim();
     var store = shiftInfo[2].trim();
-    console.log("You will be working in: " + dept + " at store #: " + store);
-    console.log("Your hours are: " + shiftHours);
+    // Split shift into begin and end time, prepend with date
+    var shift = shiftHours.split(" - ");
+    // For dates to parse, need to have a space between the time and 'AM' or 'PM', e.g. '03/19/17 3:24 PM'
+    shift.forEach(function(time, index, array) {
+      array[index] = time.slice(0, -2) + " " + time.slice(-2);
+    });
+    var shiftBegin = new Date(dates[i] + " " + shift[0]);
+    var shiftEnd = new Date(dates[i] + " " + shift[1]);
+
+    var shiftEvent = {
+      'summary': dept,
+      'location': store,
+      'start': {
+        'dateTime': shiftBegin.toISOString()
+      },
+      'end': {
+        'dateTime': shiftEnd.toISOString()
+      },
+      'reminders': {
+        'useDefault': true
+      }
+    };
+    shiftEvents.push(shiftEvent);
   }
+}
+
+// Button to import all calendar data
+var button = document.createElement("button");
+button.innerHTML = "Export to Google Calendar";
+button.onclick = insertShifts;
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+function insertShifts() {
+  shiftEvents.forEach(function(entry) {
+    var event = new CustomEvent("insertEvent", {detail: entry});
+    window.dispatchEvent(event);
+  });
 }
--- gapi_setup.js
+++ gapi_setup.js
@@ -24,17 +24,10 @@ var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+var SCOPES = "https://www.googleapis.com/auth/calendar";
 
 var authorizeButton = document.getElementById('authorize-button');
 var signoutButton = document.getElementById('signout-button');
-
-/**
- *  On load, called to load the auth2 library and API client library.
- */
-function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
-}
 
 /**
  *  Initializes the API client library and sets up sign-in state
@@ -62,7 +55,6 @@ function updateSigninStatus(isSignedIn) {
   if (isSignedIn) {
     authorizeButton.style.display = 'none';
     signoutButton.style.display = 'block';
-    listUpcomingEvents();
   } else {
     authorizeButton.style.display = 'block';
     signoutButton.style.display = 'none';
@@ -89,45 +81,41 @@ function handleSignoutClick(event) {
  *
  * @param {string} message Text to be placed in pre element.
  */
-function appendPre(message) {
+function appendPre(message, link) {
   var pre = document.getElementById('content');
-  var textContent = document.createTextNode(message + '\n');
+  var textContent = document.createTextNode('\n' + message);
+  var htmlContent = document.createElement('a');
+  htmlContent.href = link;
+  htmlContent.innerHTML = link;
+  htmlContent.target = "_blank";
+
   pre.appendChild(textContent);
+  pre.appendChild(htmlContent);
 }
 
 /**
- * Print the summary and start datetime/date of the next ten events in
- * the authorized user's calendar. If no events are found an
- * appropriate message is printed.
+ *  On load, called to load the auth2 library and API client library.
  */
-function listUpcomingEvents() {
-  gapi.client.calendar.events.list({
-    'calendarId': 'primary',
-    'timeMin': (new Date()).toISOString(),
-    'showDeleted': false,
-    'singleEvents': true,
-    'maxResults': 10,
-    'orderBy': 'startTime'
-  }).then(function(response) {
-    var events = response.result.items;
-    appendPre('Upcoming events:');
+function handleClientLoad(evt) {
+  this.CLIENT_ID = evt.detail;
+  gapi.load('client:auth2', initClient);
+}
 
-    if (events.length > 0) {
-      for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        var when = event.start.dateTime;
-        if (!when) {
-          when = event.start.date;
-        }
-        appendPre(event.summary + ' (' + when + ')');
-      }
-    } else {
-      appendPre('No upcoming events found.');
-    }
+/**
+ * Insert an event into Google Calendar
+ */
+function insertEvent(evt) {
+  var event = evt.detail;
+
+  var request = gapi.client.calendar.events.insert({
+    'calendarId': 'primary',
+    'resource': event
+  });
+
+  request.execute(function(event) {
+    appendPre('Event created: ', event.htmlLink);
   });
 }
 
-window.addEventListener("handleClientLoad", function(evt) {
-  this.CLIENT_ID = evt.detail;
-  handleClientLoad();
-}, false);
+window.addEventListener("handleClientLoad", handleClientLoad, false);
+window.addEventListener("insertEvent", insertEvent, false);
